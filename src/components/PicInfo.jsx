3d680fa442371d2fcd1455405420f8e9ab137939
lohpaul9 <lohpaul9@seas.upenn.edu>
2022-12-07 22:31:17 -0500

    component

---
+++ src/components/PicInfo.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import locationPin from '../assets/locpin.svg'
+
+const PicInfo = ({people, location, title}) => {
+  return (
+    <div className='absolute bottom-0 w-48 z-[-1]'>
+        <div className="font-spaceGrotesk font-bold text-xl flex justify-start">
+          <img src={locationPin} className='m-0'/>
+          <p className='ml-1'>{title}</p>
+        </div>
+        <p className='ml-1 font-spaceGrotesk font-normal text-s'>{location}</p>
+        <div className="flex w-full flex-wrap">{people.map(person => {
+          return <p className='px-1 py-0.5 ml-1 mb-0.5 p-1 font-spaceGrotesk font-light text-xs text-white bg-stone-400 rounded-sm'>@{person}</p>
+        })}</div>
+        
+        
+    </div>
+  )
+}
+
+export default PicInfo
--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import pin from '../assets/pin.png'
+import PicInfo from './PicInfo'
 
 const Picture = (props) => {
   return (
     <div className='relative bg-red-50'>
         <img src={pin} className='absolute m-auto left-0 right-0 w-8'/>
         <img src={props.img} className='border-4'/>
+        <PicInfo title="Hello" people = {["Paul", "Lah", "marcel", "lah", "sumanth", "lah"]} location= "Cool stuff!"/>
 
     </div>
   )
